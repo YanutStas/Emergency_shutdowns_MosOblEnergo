822ab9bcd0acbb527935e84796ed01b0612bc8d2
Stanislav <yanut995@gmail.com>
2026-07-22 15:21:49 +0300

    удалил тестовую коллекцию Strapi

--- src/api/test1/controllers/test1.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * test1 controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::test1.test1');
--- src/api/test1/routes/test1.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * test1 router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::test1.test1');
